98e5c8cdd9fe725e93b9da407f6887800388dbe0
Richard Neumann <neumann@paulmann.de>
2024-09-10 10:37:11 +0200

    Export Error

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,4 @@ use base64::{
 pub const BASE64: GeneralPurpose = GeneralPurpose::new(&STANDARD, NO_PAD);
 pub use argon2_hasher::Argon2Hasher;
 pub use keygen::Keygen;
-pub use password_verifier_ext::PasswordVerifierExt;
+pub use password_verifier_ext::{Error, PasswordVerifierExt};
