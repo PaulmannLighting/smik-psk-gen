6e2f3505832741069eba77073a63711c21f600f8
Richard Neumann <neumann@paulmann.de>
2025-01-16 10:49:01 +0100

    Update error message text

--- src/error.rs
+++ src/error.rs
@@ -12,7 +12,7 @@ pub enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::PasswordHash(error) => write!(f, "Argon2 error: {error}"),
+            Self::PasswordHash(error) => write!(f, "Password hash error: {error}"),
             Self::Base64(error) => write!(f, "Base64 error: {error}"),
         }
     }
