5aec92291bdebc878e9b7eb8d42e8192c397da10
Richard Neumann <neumann@paulmann.de>
2025-11-12 11:24:01 +0100

    Update docs.

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,12 @@ use base64::alphabet::STANDARD;
 use base64::engine::general_purpose::NO_PAD;
 use base64::engine::GeneralPurpose;
 
+/// Base64 engine without padding.
 pub const BASE64: GeneralPurpose = GeneralPurpose::new(&STANDARD, NO_PAD);
-pub const DEFAULT_KEY_SIZE: usize = 14; // 112 bits are mandatory as per EN 18031.
+
+/// Default key size in bytes for security operations.
+///
+/// # Legal compliance
+///
+/// A minimum of 112 bits is mandatory as per EN 18031.
+pub const DEFAULT_KEY_SIZE: usize = 14;
