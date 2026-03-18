e0d14d65c59fe8094f9c8843cd709c3b4d7e7cbd
Richard Neumann <neumann@paulmann.de>
2026-03-18 08:32:34 +0100

    Merge associated functions into one impl block.
    Move trait bounds into methods.

--- src/password_hash_generator.rs
+++ src/password_hash_generator.rs
@@ -20,47 +20,41 @@ impl<const SIZE: usize, R, H> PasswordHashGenerator<SIZE, R, H> {
     pub const fn new(csprng: R, hasher: H) -> Self {
         Self { csprng, hasher }
     }
-}
 
-impl<const SIZE: usize, R, H> PasswordHashGenerator<SIZE, R, H>
-where
-    R: CryptoRng,
-{
     /// Generates a random key of the specified size.
-    pub fn generate(&mut self) -> [u8; SIZE] {
+    pub fn generate(&mut self) -> [u8; SIZE]
+    where
+        R: CryptoRng,
+    {
         let mut key = [0; SIZE];
         self.csprng.fill_bytes(&mut key);
         key
     }
-}
 
-impl<const SIZE: usize, R, H> PasswordHashGenerator<SIZE, R, H>
-where
-    H: PasswordVerifier<PasswordHash>,
-{
     /// Verify a password hash.
     ///
     /// # Errors
     ///
     /// Returns an [`Error`] if the password hash could not be verified.
-    pub fn verify(&self, psk: &Psk) -> Result<(), Error> {
+    pub fn verify(&self, psk: &Psk) -> Result<(), Error>
+    where
+        H: PasswordVerifier<PasswordHash>,
+    {
         Ok(self
             .hasher
             .verify_password(&BASE64.decode(psk.base64())?, psk.hash())?)
     }
-}
 
-impl<const SIZE: usize, R, H> PasswordHashGenerator<SIZE, R, H>
-where
-    R: CryptoRng,
-    H: PasswordHasher<PasswordHash>,
-{
     /// Hash the pre-shared key.
     ///
     /// # Errors
     ///
     /// Returns a [`password_hash::Error`] if the hashing fails.
-    pub fn hash(&mut self, key: &[u8]) -> password_hash::Result<PasswordHash> {
+    pub fn hash(&mut self, key: &[u8]) -> password_hash::Result<PasswordHash>
+    where
+        R: CryptoRng,
+        H: PasswordHasher<PasswordHash>,
+    {
         self.hasher.hash_password_with_rng(&mut self.csprng, key)
     }
 }
